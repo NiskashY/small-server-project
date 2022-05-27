2a913f8044d147103be2b88b4152c7ce4b8d9b46
NiskashY <roman.pantus@mail.ru>
2022-05-27 15:12:51 +0300

    (vcpkg) add vcpkg (?)

--- cmake-build-debug/CMakeFiles/3.22.3/CMakeSystem.cmake
+++ cmake-build-debug/CMakeFiles/3.22.3/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "5.15.41-1-MANJARO")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-
+include("/home/mint/CLionProjects/Chat/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Linux-5.15.41-1-MANJARO")
 set(CMAKE_SYSTEM_NAME "Linux")
